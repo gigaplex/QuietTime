92159b7661fb8dc7339411c1219a3399fcd40e0d
Tim Boundy <tim_boundy@trimble.com>
2018-11-14 18:27:26 +1100

    Single instance mode

--- QuietTime/Program.cs
+++ QuietTime/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -8,16 +9,36 @@ namespace QuietTime
 {
     static class Program
     {
+        static bool IsProcessCurrentExe(Process process, Process currentProcess)
+        {
+            try
+            {
+                return process.MainModule.FileName == currentProcess.MainModule.FileName;
+            }
+            catch
+            {
+                // Commonly hits this path if running as a 32bit application while querying a 64bit process
+                return false;
+            }
+        }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            var appContext = new CustomApplicationContext();
-            Application.Run(appContext);
+            var currentProcess = Process.GetCurrentProcess();
+            Process[] runningProcesses = Process.GetProcessesByName(currentProcess.ProcessName)
+                .Where(process => IsProcessCurrentExe(process, currentProcess)).ToArray(); // In case of potential process name clashes, check if they're the same exe
+            
+            if (runningProcesses.Length == 1)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                var appContext = new CustomApplicationContext();
+                Application.Run(appContext);
+            }
         }
     }
 }
