84e130a1714e28c38a5185410a9ced0d6141efa2
Tim Boundy <tim_boundy@trimble.com>
2018-11-14 15:09:44 +1100

    Add project files.

---
+++ QuietTime/CustomApplicationContext.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuietTime
+{
+    class CustomApplicationContext : ApplicationContext
+    {
+        private NotifyIcon notifyIcon;
+        private SettingsForm settingsForm;
+        private VolumeHandler volumeHandler = new VolumeHandler();
+
+        public CustomApplicationContext()
+        {
+            notifyIcon = new NotifyIcon()
+            {
+                ContextMenuStrip = new ContextMenuStrip(),
+                Icon = QuietTime.Properties.Resources.NotifyIcon,
+                Text = "QuietTime",
+                Visible = true
+            };
+
+            var defaultItem = notifyIcon.ContextMenuStrip.Items.Add("Open &Settings", null, ContextMenu_OnSettings);
+            notifyIcon.ContextMenuStrip.Items.Add(new ToolStripSeparator());
+            notifyIcon.ContextMenuStrip.Items.Add("&Mute", null, ContextMenu_Mute);
+            notifyIcon.ContextMenuStrip.Items.Add("&Unmute", null, ContextMenu_Unmute);
+            notifyIcon.ContextMenuStrip.Items.Add(new ToolStripSeparator());
+            notifyIcon.ContextMenuStrip.Items.Add("&Exit", null, ContextMenu_OnExit);
+
+            defaultItem.Font = new Font(defaultItem.Font, defaultItem.Font.Style | FontStyle.Bold);
+
+            notifyIcon.MouseDoubleClick += NotifyIcon_MouseDoubleClick;
+        }
+
+        protected override void ExitThreadCore()
+        {
+            notifyIcon.Visible = false;
+            base.ExitThreadCore();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (volumeHandler != null)
+                {
+                    volumeHandler.Dispose();
+                    volumeHandler = null;
+                }
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private void ShowSettingsForm()
+        {
+            if (settingsForm == null)
+            {
+                settingsForm = new SettingsForm();
+                settingsForm.Closed += SettingsForm_Closed;
+                settingsForm.Show();
+            }
+            else
+            {
+                settingsForm.Activate();
+            }
+        }
+
+        private void ContextMenu_OnSettings(object sender, EventArgs e)
+        {
+            ShowSettingsForm();
+        }
+
+        private void ContextMenu_Mute(object sender, EventArgs e)
+        {
+            volumeHandler.Mute();
+        }
+
+        private void ContextMenu_Unmute(object sender, EventArgs e)
+        {
+            volumeHandler.Unmute();
+        }
+
+        private void ContextMenu_OnExit(object sender, EventArgs e)
+        {
+            ExitThread();
+        }
+
+        private void NotifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ShowSettingsForm();
+        }
+
+        private void SettingsForm_Closed(object sender, EventArgs e)
+        {
+            settingsForm = null;
+        }
+    }
+}
---
+++ QuietTime/VolumeHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.CoreAudioApi;
+
+namespace QuietTime
+{
+    class VolumeHandler : IDisposable
+    {
+        private MMDevice audioDevice;
+
+        public VolumeHandler()
+        {
+            using (var mmDeviceEnumerator = new MMDeviceEnumerator())
+            {
+                audioDevice = mmDeviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            }
+        }
+
+        public void Mute()
+        {
+            audioDevice.AudioEndpointVolume.Mute = true;
+        }
+
+        public void Unmute()
+        {
+            audioDevice.AudioEndpointVolume.Mute = false;
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    if (audioDevice != null)
+                    {
+                        audioDevice.Dispose();
+                        audioDevice = null;
+                    }
+                }
+
+                disposedValue = true;
+            }
+        }
+        
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+        }
+        #endregion
+    }
+}
